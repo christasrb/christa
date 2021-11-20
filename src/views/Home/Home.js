7420162a0bc09d9fb52baec0b30d996d03429383
christasrb <christasurbakti@gmail.com>
2021-11-20 15:28:00 +0700

    Tugas Pertemuan 6

---
+++ src/views/Home/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class Home extends React.Component{
+
+constructor(props){
+    super(props);
+    this.state = {name : this.props.name}
+}
+
+render () {
+    return(
+        <div>
+           <h2>Title Home Componenet</h2> 
+           <p>Hello my name is {this.state.name}</p>
+           <button onClick={()=> this.setState({name : 'Tyrion Lannister'})}>Ubah Nama</button>
+        </div>
+    )
+}
+    
+
+    // render() {
+    //     return(
+    //         <div>
+    //             <h2>Title Home Component</h2>
+    //             <p>Hello my name is {this.props.name}</p>
+    //         </div>
+    //     )
+    // }
+}
